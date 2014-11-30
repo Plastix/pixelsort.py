328ac1b21a145d221e5d03aad7744a1d31021b38
Plastix <aidanpieper@gmail.com>
2014-11-30 00:04:04 -0500

    Complete rewrite for speed using NumPy and OpenCV Goodbye Pillow...I never liked you anyway!

--- pixelsort.py
+++ pixelsort.py
@@ -1,63 +1,70 @@
 #!/usr/bin/env python
 
-from PIL import Image
+import cv2
+import numpy
 import time
 
 
 def sort_by_row(image):
     """
     Sorts each row of the specified image
-    Modifies the image object in place.
+    Returns the sorted image.
     """
-    data = image.load()
-    width, height = image.size
-    for j in range(height):
-        splice = sorted([data[i, j] for i in range(width)])
-        for i in range(width):
-            data[i, j] = splice[i]
+    return numpy.sort(image, axis=1)
 
 
 def sort_by_column(image):
     """
-    Sorts each column of the specified row.
-    Modifies the image object in place.
+    Sorts each column of the specified image.
+    Returns the sorted image.
     """
-    data = image.load()
-    width, height = image.size
-    for j in range(width):
-        splice = sorted([data[j, i] for i in range(height)])
-        for i in range(height):
-            data[j, i] = splice[i]
+    return numpy.sort(image, axis=0)
 
 
 def sort_by_npx(image, numpx=50):
     """
-    Treats the specified image data as a 1D list and s`orts by every N pixels.
-    Modifies the image object in place.
+    Splits the image into segments of N pixels and sorts each one
+    Returns the sorted image.
     """
-    data = list(image.getdata())  # Pull image data and convert to list
-    for i in range(0, len(data) - numpx, numpx):
-        section = sorted(data[i + j] for j in range(numpx))
-        for k in range(numpx):
-            data[i + k] = section[k]
-    image.putdata(data)  # Push the sorted data back to the original image
+    depth = image.shape[2]
+    # Convert the 3D array into 2D
+    # Squeeze trims the enapsulating arrays left over from reshape
+    flat = numpy.reshape(image, (-1, 1, depth)).squeeze()
+    new_width = flat.shape[0]
+    remainder = new_width % numpx
+    split_index = new_width - remainder
+
+    # Transform the 2D array into a 3D array of N pixel segments
+    # Because N pixels won't evenly divide into our array evenly every time we
+    # have to split the array at split_index and sort the other part separately
+    reshaped = numpy.reshape(flat[:split_index], (-1, numpx, depth))
+    sort1 = numpy.sort(reshaped, axis=1)
+    sort1 = numpy.reshape(sort1, (-1, 1, depth)).squeeze()
+
+    sort2 = numpy.sort(flat[split_index:], axis=1)
+
+    # Combine the parts back together and reshape back into original image size
+    sort_final = numpy.vstack((sort1, sort2))
+    return numpy.reshape(sort_final, image.shape)
+
 
 def sort_by_sumrgb(image):
     """
     Sorts by the sum of red, green, and blue values of the pixels
+    Returns the sorted image.
     """
-    data = list(image.getdata())
-    sort = sorted([(sum(i), i) for i in data])
-    for i in range(len(sort)):
-        data[i] = sort[i][1]
-    image.putdata(data)
+    flat = numpy.reshape(image, (-1, 1, image.shape[2])).squeeze()
+    sums = numpy.sum(flat, axis=1).reshape((-1, 1))
+    sort = numpy.sort(numpy.hstack((sums, flat)), axis=0)
+    final = numpy.delete(sort, numpy.s_[:1:1], axis=1)
+    return numpy.reshape(final, image.shape)
 
 
 if __name__ == '__main__':
-    image = Image.open('yukon.jpg')
+    image = cv2.imread('yukon.jpg')
     start = time.clock()
-    sort_by_column(image)
+    image = sort_by_npx(image)
     end = time.clock()
     elapsed = '%.2f' % (end - start)
     print 'Image Sorted in ' + elapsed + ' seconds!'
-    image.save('output.' + image.format.lower())
+    cv2.imwrite('output.jpg', image)
