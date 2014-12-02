3aa255d34f4860603ed004f09bb437806d059771
Plastix <aidanpieper@gmail.com>
2014-12-02 13:58:39 -0500

    Fix I/O options and tidy up doc strings

--- pixelsort.py
+++ pixelsort.py
@@ -7,32 +7,33 @@ import time
 
 
 @click.group()
-@click.option('-i', "--input", nargs=1, default="Lenna.png",
-              help="File to pixelsort", type=click.Path(exists=True))
-@click.option('-o', "--output", nargs=1, default="output.png", help="File to output")
+@click.option('-i', '--src', nargs=1, default='Lenna.png',
+              help='Input file to pixelsort.', type=click.Path(exists=True))
+@click.option('-o', '--out', nargs=1, default='output.png',
+              help='Sorted file to output.')
 @click.version_option(version=0.1)
 @click.pass_context
 def cli(ctx, src, out):
-    """Sort pixels from the CLI"""
-    ctx.obj['src'] = cv2.imread(src)
-    ctx.obj['out'] = out
+    """Sort pixels from the CLI."""
+    ctx.obj['i'] = cv2.imread(src)
+    ctx.obj['o'] = out
     ctx.obj['start'] = time.clock()
 
 
 @cli.command()
 @click.pass_context
 def column(ctx):
-    """Sorts each column of the image"""
-    sort = sorts.sort_by_column(ctx.obj['src'])
-    save_image(ctx.obj['out'], sort, ctx.obj['start'])
+    """Sorts each column of the image."""
+    sort = sorts.sort_by_column(ctx.obj['i'])
+    save_image(ctx.obj['o'], sort, ctx.obj['start'])
 
 
 @cli.command()
 @click.pass_context
 def row(ctx):
-    """Sorts each row of the image"""
-    sort = sorts.sort_by_row(ctx.obj['src'])
-    save_image(ctx.obj['out'], sort, ctx.obj['start'])
+    """Sorts each row of the image."""
+    sort = sorts.sort_by_row(ctx.obj['i'])
+    save_image(ctx.obj['o'], sort, ctx.obj['start'])
 
 
 @cli.command()
@@ -40,17 +41,17 @@ def row(ctx):
                 metavar="<Number of Pixels>", default=50)
 @click.pass_context
 def npixel(ctx, numpx):
-    """Sorts every N-pixels of the image"""
-    sort = sorts.sort_by_npx(ctx.obj['src'], numpx)
-    save_image(ctx.obj['out'], sort, ctx.obj['start'])
+    """Sorts every N-pixels of the image."""
+    sort = sorts.sort_by_npx(ctx.obj['i'], numpx)
+    save_image(ctx.obj['o'], sort, ctx.obj['start'])
 
 
 @cli.command()
 @click.pass_context
 def sumrgb(ctx):
-    """Sorts by sum of the RGB channels"""
-    sort = sorts.sort_by_sumrgb(ctx.obj['src'])
-    save_image(ctx.obj['out'], sort, ctx.obj['start'])
+    """Sorts by sum of the RGB channels."""
+    sort = sorts.sort_by_sumrgb(ctx.obj['i'])
+    save_image(ctx.obj['o'], sort, ctx.obj['start'])
 
 
 def save_image(filename, data, start):
